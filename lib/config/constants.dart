0be2ca85da8a50a6e67003912bf47f55f035e41e
Carlos Ormeno <carlos.ormeno@outlook.es>
2025-10-20 08:56:46 -0500

    test

--- lib/config/constants.dart
+++ lib/config/constants.dart
@@ -14,6 +14,7 @@ class Constants {
       return 'http://10.0.2.2:8080/api'; //Trabaja para el emulador
       //return 'http://172.20.10.2:8080/api';
       //return 'http://192.168.137.1:8080/api';  //Trabaja para la Wifi que comparte el equipo
+      //otro cambios
     }
     return 'http://localhost:8080/api';
   }
